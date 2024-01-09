8a99fd27c68719202a102961a107f78908ce51d3
harck5 <rauljimenezhk@gmail.com>
2024-01-10 00:07:02 +0100

    10/1/2024
    Movimiento player prematuro
    noria normal e invertida
    giratorio normal e invertido

---
+++ Assets/Proyect/Script/Plataforms/ContraRotatingPlataforms.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContraRotatingPlataforms : MonoBehaviour
+{
+
+    private float angle = 0, currentTime;
+    public float speed = 5;
+
+    void Update()
+    {
+        currentTime = Time.time;
+        angle = currentTime;
+        transform.Rotate(Vector3.forward * speed / -10);
+
+        if (angle >= 360f)
+        {
+            angle = 0f;
+        }
+    }
+}
---
+++ Assets/Proyect/Script/Player/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float force, jumpForce, reduceForce;
+    private float xInput;
+    private Rigidbody playerRb;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (isGrounded)
+        {
+            xInput /= 2;
+        }
+            xInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 input = new Vector3(xInput, 0, 0);
+        playerRb.AddForce(input * force);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
