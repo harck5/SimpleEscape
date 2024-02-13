4f5a37450b362f98f5f78a431592b6cdfe24a91f
harck5 <rauljimenezhk@gmail.com>
2024-02-13 20:33:07 +0100

    a

--- Assets/Proyect/Script/Player/Pruebas.cs
+++ Assets/Proyect/Script/Player/Pruebas.cs
@@ -26,13 +26,46 @@ public class Pruebas : MonoBehaviour
         //canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerRadius, playerRadius, transform.right, moveDistance);
         
 
-        canMove = !Physics.Raycast(transform.position, Vector3.right, playerRadius + moveDistance) &&
-                  !Physics.Raycast(transform.position, Vector3.left, playerRadius - moveDistance);
+        canMove = !Physics.Raycast(transform.position, Vector3.right, playerRadius + moveDistance, LayerMask.NameToLayer("Ignore Raycast")) &&
+                  !Physics.Raycast(transform.position, Vector3.left, playerRadius - moveDistance, LayerMask.NameToLayer("Ignore Raycast"));
         if (Input.GetKeyDown(KeyCode.LeftShift)) { speed = 7; }
         if (Input.GetKeyUp(KeyCode.LeftShift)) { speed = 5; }
         canJump = !Physics.Raycast(transform.position, Vector3.down, playerRadius + 0.1f);
         Debug.Log(canJump);
         if (canMove) { transform.Translate(Vector3.right * moveDistance); }
         if(!canJump && Input.GetKeyDown(KeyCode.Space)) { playerRigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse); }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = false;//Can jump
+            //Parent the player to the platform, in case it is necessary for the player to follow the movement of the platforms
+            transform.parent = collision.transform;
+        }
+        if (collision.gameObject.CompareTag("BouncyGround"))
+        {
+            //It is the same line that uses space to jump
+            playerRigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        }
+        if (collision.gameObject.CompareTag("RotatingGround"))
+        {
+            canJump = false;//Can jump
+        }
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = false;//Can jump
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;//Can't jump
+            transform.parent = null;//Unparent the player to plataform
+        }
     }
 }
